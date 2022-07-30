8bc04864ef30820448ff77c7ccd1b92dc781bdf8
Jasper Pol <jpolvto@gmail.com>
2022-07-30 23:17:01 +0200

    making a self-consistent field application instead

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,16 @@
-use std::{env, error::Error};
-use bson::Document;
-use mongodb::{Client, Collection};
-use mongodb::options::ClientOptions;
-use serde::{Serialize, Deserialize};
-use csv;
-use serde_json::json;
+use std::env;
+use std::fs;
 
-use tokio;
+use nalgebra::DMatrix;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct User {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub id: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub email: Option<String>
-}
-
-
-fn get_users_from_csv(path: String) -> Result<Vec<User>, Box<dyn Error>> {
-    let mut users: Vec<User> = vec![];
-    let mut rdr = csv::Reader::from_path(path)?;
-    for result in rdr.deserialize() {
-        let user: User = result?;
-        users.push(user);
-    }
-    Ok(users)
-}
-
-#[tokio::main]
-async fn main() {
+fn main() {
     dotenv::dotenv().ok();
 
-    let database_name = env::var("DATABASE_NAME").unwrap();
-    let password = env::var("PASSWORD").unwrap();
-    let user = env::var("USER").unwrap();
-    let file_name = env::var("FILE_NAME").unwrap();
+    let file_name = env::var("SELF_CONSISTENT_FIELD_FILE_NAME").unwrap();
+    let contents = fs::read_to_string(file_name).unwrap();
+    let deserialized: DMatrix<f32> = serde_json::from_str(&contents).unwrap();
+    let bla = deserialized.eigenvalues().unwrap();
 
-    let host_name = format!("mongodb+srv://{}:{}@cluster0.17s4f.mongodb.net/retryWrites=true&w=majority",
-                            &user,
-                            &password,
-    );
-
-    let options = ClientOptions::parse(&host_name).await.unwrap();
-    let client = Client::with_options(options).unwrap();
-    let db = client.database(&database_name);
-
-    let users = get_users_from_csv(file_name).unwrap();
-    let mut docs: Vec<Document> = Vec::new();
-
-    for user in users {
-        let serialized_user = bson::to_bson(&user).unwrap();  // Serialize
-        if let bson::Bson::Document(document) = serialized_user {
-            docs.push(document)
-        }
-    }
-
-    let user_collection: Collection<Document> = db.collection("users");
-    let result =  user_collection.insert_many(docs, None).await.unwrap();
-
-    println!("{}", json!(result))
+    // Compare against reference
+    println!("Computed eigenvalues:\n{}", bla);
 }
