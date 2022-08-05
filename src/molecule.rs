5478ca6c5fad01c18cefbc3db32086c4f5b716c1
Jasper Pol <jpolvto@gmail.com>
2022-08-05 16:10:58 +0200

    that's too many casts, I should change this

--- src/molecule.rs
+++ src/molecule.rs
@@ -2,23 +2,13 @@ use nalgebra::Vector3;
 use serde::{Serialize, Deserialize};
 use statrs::function::erf::erf;
 
-const PI:f64 = 3.141592654;
+const PI: f64 = 3.141592654;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Atom {
     atomic_number: i32,
-    position: Vector3<i32>,
+    position: Vector3<f64>,
     number_of_electrons: i32
-}
-
-impl Atom {
-    fn get_position(&self) -> Vector3<i32> {
-        return self.position
-    }
-
-    fn get_atomic_number(&self) -> i32 {
-        return self.atomic_number;
-    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -47,13 +37,13 @@ impl Molecule {
         
     }
 
-    fn get_nuclear_repulsion_energy(&self) -> i32 {
+    fn get_nuclear_repulsion_energy(&self) -> f64 {
 
-        let mut energy:i32 = 0;
+        let mut energy: f64 = 0.0;
 
         for atom1 in 0..self.atoms.len() {
             for atom2 in (atom1 + 1)..self.atoms.len() {
-                energy += self.atoms[atom1].z / (self.atoms[atom1].position - self.atoms[atom2].position).len();
+                energy += (self.atoms[atom1].atomic_number) as f64 / (self.atoms[atom1].position - self.atoms[atom2].position).norm();
             } 
         }
         return energy;
@@ -65,30 +55,29 @@ impl Molecule {
 
 }
 
-fn gaussian_product (gaussian_a:(i32, Vector3<i32>), gaussian_b:(i32, Vector3<i32>,)) -> (i32, i32, i32, Vector3<i32>) {
+fn gaussian_product (gaussian_a:(f64, Vector3<f64>), gaussian_b:(f64, Vector3<f64>,)) -> (f64, f64, f64, Vector3<f64>) {
     let (a, position_a) = gaussian_a;
     let (b, position_b) = gaussian_b;
     
     let p = a + b;
     let difference = (position_a - position_b).norm_squared();
-    let n = (4 * a * b / (PI.powf(2))).pow(0.75);
-    let k = n * (-a * b / p * difference).exp;
+    let k = (4.0 * a * b / (PI.powf(2.0))).powf(0.75) * (-a * b / p * difference).exp();
     let position_p = (a * position_a + b * position_b) / p;
 
     (p, difference, k, position_p)
 }
 
-fn overlap_integral (orbital_a:(i32, Vector3<i32>), orbital_b:(i32, Vector3<i32>)) {
+fn overlap_integral (orbital_a:(f64, Vector3<f64>), orbital_b:(f64, Vector3<f64>)) {
     let (p, difference, k, position_p) = gaussian_product(orbital_a, orbital_b);
     (PI / position_p).pow(1.5) * k
 }
 
-fn kinetic_integral (orbital_a:(i32, Vector3<i32>), orbital_b:(i32, Vector3<i32>)) -> i32 {
+fn kinetic_integral (orbital_a:(f64, Vector3<f64>), orbital_b:(f64, Vector3<f64>)) -> f64 {
     let (p, difference, k, position_p) = gaussian_product(orbital_a, orbital_b);
     let (a, position_a) = orbital_a;
     let (b, position_b) = orbital_b;
 
-    (a * b / p) * (3 - 2 * (a * b / p) * difference) * (PI / p).pow(1.5) * k
+    (a * b / p) * (3.0 - 2.0 * (a * b / p) * difference) * (PI / p).powf(1.5) * k
 }
 
 fn error_function(t: f64) -> f64 {
@@ -99,18 +88,16 @@ fn error_function(t: f64) -> f64 {
     }
 }
 
-fn potential_integral (gaussian_a:(i32, Vector3<i32>),gaussian_b:(i32, Vector3<i32>),atom_index: usize, molecule: Molecule) {
+fn potential_integral (gaussian_a:(f64, Vector3<f64>),gaussian_b:(f64, Vector3<f64>),atom_index: usize, molecule: Molecule) {
     let (p, difference, k, position) = gaussian_product(gaussian_a, gaussian_b);
-    let atom_position = molecule.atoms[atom_index].position;
-    let atomic_number = molecule.atoms[atom_index].atomic_number;
 
-    (-2 * PI * atomic_number / position) * k * error_function(p * (position - atom_position).norm_squared())
+    (-2.0 * PI * molecule.atoms[atom_index].atomic_number as f64 / position) * k * error_function((p * (position - molecule.atoms[atom_index].position).norm_squared()).into())
 }
 
-fn multi_electron_integral (gaussian_a:(i32, Vector3<i32>), gaussian_b:(i32, Vector3<i32>), gaussian_c:(i32, Vector3<i32>), gaussian_d:(i32, Vector3<i32>)) -> Vector3<i32> {
+fn multi_electron_integral (gaussian_a:(f64, Vector3<f64>), gaussian_b:(f64, Vector3<f64>), gaussian_c:(f64, Vector3<f64>), gaussian_d:(f64, Vector3<f64>)) -> f64 {
     let (p, difference_ab, k_ab, position_p) = gaussian_product(gaussian_a, gaussian_b);
     let (q, difference_cd, k_cd, position_q) = gaussian_product(gaussian_c, gaussian_d);
 
-    2 * PI.powf(2.5) * (p * q * (p + q).pow(0.5).pow(-1)) * k_ab * k_cd * error_function(p * q / (p + q) * (position_p - position_q).norm_squared())
+    2.0 * PI.powf(2.5) * 1.0 / (p * q * (p + q)).sqrt() * k_ab * k_cd * error_function((p * q / (p + q) * (position_p - position_q).norm_squared()).into())
 }
 
