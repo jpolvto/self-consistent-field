69d008a867039a3e272627322fba1f82c42d2e13
Jasper Pol <jpolvto@gmail.com>
2022-08-05 21:06:26 +0200

    it compiles, but the calculations need to be checked, and the methods restructured

--- src/molecule.rs
+++ src/molecule.rs
@@ -1,14 +1,15 @@
 use nalgebra::Vector3;
 use serde::{Serialize, Deserialize};
-use statrs::function::erf::erf;
+use fastapprox::faster::erf;
 
-const PI: f64 = 3.141592654;
+const PI: f32 = 3.141592654;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Atom {
     atomic_number: i32,
-    position: Vector3<f64>,
-    number_of_electrons: i32
+    position: Vector3<f32>,
+    number_of_electrons: i32,
+    principal_quantum_number: i32
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -17,45 +18,47 @@ pub struct Molecule {
 }
 
 impl Molecule {
-    fn get_max_angular_momentum(&self) {
-        
+    fn get_overlap_integral(orbital_a:(f32, Vector3<f32>), orbital_b:(f32, Vector3<f32>)) -> f32 {
+        let (p, difference, k, position_p) = gaussian_product(orbital_a, orbital_b);
+        (PI / p).powf(1.5) * k
+    }
+    
+    fn get_kinetic_integral(orbital_a:(f32, Vector3<f32>), orbital_b:(f32, Vector3<f32>)) -> f32 {
+        let (p, difference, k, position_p) = gaussian_product(orbital_a, orbital_b);
+        let (a, position_a) = orbital_a;
+        let (b, position_b) = orbital_b;
+    
+        (a * b / p) * (3.0 - 2.0 * (a * b / p) * difference) * (PI / p).powf(1.5) * k
     }
 
-    fn get_number_of_contracted_gaussians(&self) {
-        
+    fn get_potential_integral(gaussian_a:(f32, Vector3<f32>),gaussian_b:(f32, Vector3<f32>),atom_index: usize, molecule: Molecule) -> f32 {
+        let (p, difference, k, position) = gaussian_product(gaussian_a, gaussian_b);
+    
+        (-2.0 * PI * molecule.atoms[atom_index].atomic_number as f32 / p) * k * error_function((p * (position - molecule.atoms[atom_index].position).norm_squared()).into())
+    }
+    
+    fn get_multi_electron_integral(gaussian_a:(f32, Vector3<f32>), gaussian_b:(f32, Vector3<f32>), gaussian_c:(f32, Vector3<f32>), gaussian_d:(f32, Vector3<f32>)) -> f32 {
+        let (p, difference_ab, k_ab, position_p) = gaussian_product(gaussian_a, gaussian_b);
+        let (q, difference_cd, k_cd, position_q) = gaussian_product(gaussian_c, gaussian_d);
+    
+        2.0 * PI.powf(2.5) * 1.0 / (p * q * (p + q)).sqrt() * k_ab * k_cd * error_function((p * q / (p + q) * (position_p - position_q).norm_squared()).into())
     }
 
-    fn get_number_of_gaussians(&self) {
-        
-    }
+    fn get_nuclear_repulsion_energy(&self) -> f32 {
 
-    fn get_alpha_electrons(&self) {
-        
-    }
-
-    fn get_beta_electrons(&self) {
-        
-    }
-
-    fn get_nuclear_repulsion_energy(&self) -> f64 {
-
-        let mut energy: f64 = 0.0;
+        let mut energy: f32 = 0.0;
 
         for atom1 in 0..self.atoms.len() {
             for atom2 in (atom1 + 1)..self.atoms.len() {
-                energy += (self.atoms[atom1].atomic_number) as f64 / (self.atoms[atom1].position - self.atoms[atom2].position).norm();
+                energy += (self.atoms[atom1].atomic_number) as f32 / (self.atoms[atom1].position - self.atoms[atom2].position).norm();
             } 
         }
         return energy;
     }
 
-    fn get_number_of_electrons(&self) {
-
-    }
-
 }
 
-fn gaussian_product (gaussian_a:(f64, Vector3<f64>), gaussian_b:(f64, Vector3<f64>,)) -> (f64, f64, f64, Vector3<f64>) {
+fn gaussian_product (gaussian_a:(f32, Vector3<f32>), gaussian_b:(f32, Vector3<f32>,)) -> (f32, f32, f32, Vector3<f32>) {
     let (a, position_a) = gaussian_a;
     let (b, position_b) = gaussian_b;
     
@@ -67,37 +70,10 @@ fn gaussian_product (gaussian_a:(f64, Vector3<f64>), gaussian_b:(f64, Vector3<f6
     (p, difference, k, position_p)
 }
 
-fn overlap_integral (orbital_a:(f64, Vector3<f64>), orbital_b:(f64, Vector3<f64>)) {
-    let (p, difference, k, position_p) = gaussian_product(orbital_a, orbital_b);
-    (PI / position_p).pow(1.5) * k
-}
-
-fn kinetic_integral (orbital_a:(f64, Vector3<f64>), orbital_b:(f64, Vector3<f64>)) -> f64 {
-    let (p, difference, k, position_p) = gaussian_product(orbital_a, orbital_b);
-    let (a, position_a) = orbital_a;
-    let (b, position_b) = orbital_b;
-
-    (a * b / p) * (3.0 - 2.0 * (a * b / p) * difference) * (PI / p).powf(1.5) * k
-}
-
-fn error_function(t: f64) -> f64 {
+fn error_function(t: f32) -> f32 {
     if t == 0.0 {
          1.0
     } else {
         (0.5 * (PI / t).powf(0.5)) * erf(t.powf(0.5))
     }
 }
-
-fn potential_integral (gaussian_a:(f64, Vector3<f64>),gaussian_b:(f64, Vector3<f64>),atom_index: usize, molecule: Molecule) {
-    let (p, difference, k, position) = gaussian_product(gaussian_a, gaussian_b);
-
-    (-2.0 * PI * molecule.atoms[atom_index].atomic_number as f64 / position) * k * error_function((p * (position - molecule.atoms[atom_index].position).norm_squared()).into())
-}
-
-fn multi_electron_integral (gaussian_a:(f64, Vector3<f64>), gaussian_b:(f64, Vector3<f64>), gaussian_c:(f64, Vector3<f64>), gaussian_d:(f64, Vector3<f64>)) -> f64 {
-    let (p, difference_ab, k_ab, position_p) = gaussian_product(gaussian_a, gaussian_b);
-    let (q, difference_cd, k_cd, position_q) = gaussian_product(gaussian_c, gaussian_d);
-
-    2.0 * PI.powf(2.5) * 1.0 / (p * q * (p + q)).sqrt() * k_ab * k_cd * error_function((p * q / (p + q) * (position_p - position_q).norm_squared()).into())
-}
-
