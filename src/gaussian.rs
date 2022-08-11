5f2173315c4f0e5bc89bb1268aa7ba8fec20ee60
Jasper Pol <jpolvto@gmail.com>
2022-08-11 22:37:47 +0200

    bit of formatting, now I need to take a dive into the formulas

--- src/gaussian.rs
+++ src/gaussian.rs
@@ -17,12 +17,14 @@ impl Gaussian {
         let mut wavefunction: f32 = Default::default();
     
         for i in 0..n {
-            wavefunction += b.elements.get(&atomic_number).unwrap().electron_shells[usize::MIN].coefficients[usize::MIN][i as usize] * ((2.0 * b.elements.get(&atomic_number).unwrap().electron_shells[usize::MIN].exponents[i as usize]) / PI).powf(0.75) * (-b.elements.get(&atomic_number).unwrap().electron_shells[usize::MIN].exponents[i as usize] * r.powf(2.0)).exp();
+            wavefunction += b.elements.get(&atomic_number).unwrap().electron_shells[usize::MIN].coefficients[usize::MIN][i as usize] * 
+            ((2.0 * b.elements.get(&atomic_number).unwrap().electron_shells[usize::MIN].exponents[i as usize]) / PI).powf(0.75) * 
+            (-b.elements.get(&atomic_number).unwrap().electron_shells[usize::MIN].exponents[i as usize] * r.powf(2.0)).exp();
         }
         return wavefunction
     }
 
-    pub fn gaussian_product (a: &Gaussian, b: &Gaussian) -> (Gaussian, f32, f32) {
+    fn gaussian_product(a: &Gaussian, b: &Gaussian) -> (Gaussian, f32, f32) {
 
         let difference = (a.center - b.center).norm_squared();
         let exponent = a.exponent + b.exponent;
@@ -41,20 +43,24 @@ impl Gaussian {
     fn get_kinetic_integral(a: &Gaussian, b: &Gaussian) -> f32 {
         let (c, difference, k) = Gaussian::gaussian_product(a, b);
         
-        (a.exponent * b.exponent / c.exponent) * (3.0 - 2.0 * (a.exponent * b.exponent / c.exponent) * difference) * (PI / c.exponent).powf(1.5) * k
+        (a.exponent * b.exponent / c.exponent) * (3.0 - 2.0 * 
+        (a.exponent * b.exponent / c.exponent) * difference) * (PI / c.exponent).powf(1.5) * k
     }
 
     fn get_potential_integral(a: &Gaussian,b: &Gaussian, atom: Atom) -> f32 {
         let (c, difference, k) = Gaussian::gaussian_product(a, b);
 
-        (-2.0 * PI * atom.atomic_number as f32 / c.exponent) * k * Gaussian::error_function((c.exponent * (c.center - atom.position).norm_squared()).into())
+        (-2.0 * PI * atom.atomic_number as f32 / c.exponent) * k * 
+        Gaussian::error_function((c.exponent * (c.center - atom.position).norm_squared()).into())
     }
     
     fn get_multi_electron_integral(a: &Gaussian, b: &Gaussian, c: &Gaussian, d: &Gaussian) -> f32 {
         let (p, difference_ab, k_ab) = Gaussian::gaussian_product(a, b);
         let (q, difference_cd, k_cd) = Gaussian::gaussian_product(c, d);
     
-        2.0 * PI.powf(2.5) * 1.0 / (p.exponent * q.exponent * (p.exponent + q.exponent)).sqrt() * k_ab * k_cd * Gaussian::error_function((p.exponent * q.exponent / (p.exponent + q.exponent) * (p.center - q.center).norm_squared()).into())
+        2.0 * PI.powf(2.5) * 1.0 / (p.exponent * q.exponent * (p.exponent + q.exponent)).sqrt() * k_ab * k_cd * 
+        Gaussian::error_function((p.exponent * q.exponent / (p.exponent + q.exponent) * 
+        (p.center - q.center).norm_squared()).into())
     }
 
     fn error_function(t: f32) -> f32 {
